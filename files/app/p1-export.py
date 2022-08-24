8640f4c39beded357ba4b5c37e3b8e8db5de988a
Sander Revenberg <sander@revenberg.info>
2022-08-24 08:39:27 +0200

    .

--- files/app/p1-export.py
+++ files/app/p1-export.py
@@ -220,10 +220,10 @@ class AppMetrics:
             LOG.info(self.meter._datadetails[record]['value'])
             LOG.info(self.meter._datadetails[record]['OBIS_reference'])
             self.prometheus = []
-            if self.meter._datadetails[record].prometheus == "Info":
-                self.prometheus[self.meter._datadetails[record].OBIS_reference] = Info(PROMETHEUS_PREFIX + self.meter._datadetails[record].prometheus, self.meter._datadetails[record].value)
-            if self.meter._datadetails[record].prometheus == "Gauge":
-                self.prometheus[self.meter._datadetails[record].OBIS_reference] = Gauge(PROMETHEUS_PREFIX + self.meter._datadetails[record].prometheus, self.meter._datadetails[record].value)
+            if self.meter._datadetails[record]['prometheus'] == "Info":
+                self.prometheus[self.meter._datadetails[record]['OBIS_reference']] = Info(PROMETHEUS_PREFIX + self.meter._datadetails[record]['prometheus'], self.meter._datadetails[record]['value'])
+            if self.meter._datadetails[record]['prometheus'] == "Gauge":
+                self.prometheus[self.meter._datadetails[record]['OBIS_reference']] = Gauge(PROMETHEUS_PREFIX + self.meter._datadetails[record]['prometheus'], self.meter._datadetails[record]['value'])
         
     def run_metrics_loop(self):
         """Metrics fetching loop"""
